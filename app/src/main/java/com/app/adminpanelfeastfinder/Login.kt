059b5887b97e0500b928803ec0c497f85d1db802
Dell <syeduzair99jamal@gmail.com>
2024-03-22 03:09:21 +0500

    dashboard screen in progress.

--- app/src/main/java/com/app/adminpanelfeastfinder/Login.kt
+++ app/src/main/java/com/app/adminpanelfeastfinder/Login.kt
@@ -1,11 +1,22 @@
 package com.app.adminpanelfeastfinder
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.app.adminpanelfeastfinder.databinding.ActivityLoginBinding
 
 class Login : AppCompatActivity() {
+    private lateinit var binding: ActivityLoginBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.loginBtn.setOnClickListener {
+            startActivity(Intent(this, SignUp::class.java))
+        }
+        binding.noAcc.setOnClickListener {
+            startActivity(Intent(this, SignUp::class.java))
+        }
     }
 }
--- app/src/main/java/com/app/adminpanelfeastfinder/SignUp.kt
+++ app/src/main/java/com/app/adminpanelfeastfinder/SignUp.kt
@@ -1,11 +1,27 @@
 package com.app.adminpanelfeastfinder
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import com.app.adminpanelfeastfinder.databinding.ActivitySignUpBinding
 
 class SignUp : AppCompatActivity() {
+    private lateinit var binding: ActivitySignUpBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_sign_up)
+        binding = ActivitySignUpBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.createAcc.setOnClickListener {
+            startActivity(Intent(this, MainActivity::class.java))
+        }
+        binding.haveAcc.setOnClickListener {
+            startActivity(Intent(this,Login::class.java))
+        }
+        val locationList = arrayOf("Karachi","Lahore","Islamabad","Quetta","Multan","Hyderabad","Peshawar")
+        val adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1,locationList)
+        val autoCompleteText = binding.listOfLoc
+        autoCompleteText.setAdapter(adapter)
     }
 }
